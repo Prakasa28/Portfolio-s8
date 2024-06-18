9b92882839629d460caf4477c939510ea952c1cd
Prakasa K S <prakasa.ks@bycape.io>
2024-06-18 01:01:56 +0100

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import GroupProject from "./Pages/GroupProject";
 import Index from "./Pages/index";
-import IndividualProject from "./Pages/IndividualProject";
-import InternationalProject from "./Pages/InternationalProject";
 import GraduationProject from "./Pages/GraduationProject";
 
 function App() {
@@ -10,16 +7,9 @@ function App() {
     <Router>
       <Routes>
         {/* Other routes */}
-        <Route path="/groupProject" element={<GroupProject />} />
         <Route path="/" element={<Index />} />
-        <Route path="/individualProject" element={<IndividualProject />} />
-        <Route
-          path="/internationalProject"
-          element={<InternationalProject />}
-        />
-        <Route path={"/graduationProject"} element={<GraduationProject/>}/>
+        <Route path={"/graduationProject"} element={<GraduationProject />} />
       </Routes>
-      
     </Router>
   );
 }
